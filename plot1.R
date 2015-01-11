cf3a1a19a50c696f53dccbd357a60313e2aec827
Dmitry Nikitin <dminikitin@gmail.com>
2015-01-11 04:17:38 -0500

    added title and axis labels to first plot

--- plot1.R
+++ plot1.R
@@ -1,2 +1,7 @@
 source("readData.R")
-hist(dataTable$Global_active_power, col="red")
+hist(
+  dataTable$Global_active_power, 
+  col="red",
+  main="Global Active Power", 
+  xlab="Global Active Power (kilowatts)", 
+  ylab="Frequency")
