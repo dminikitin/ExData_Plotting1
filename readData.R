d2b7678e5b2016383752c56579567b062cbd5fff
Dmitry Nikitin <dminikitin@gmail.com>
2015-01-11 03:14:09 -0500

    read data using fread as it's much faster

--- readData.R
+++ readData.R
@@ -1,35 +1,48 @@
-colClasses_fread<-c("DateDDMMYYYY", "TimeString", "NumericOptional", "NumericOptional", "NumericOptional", "NumericOptional", "NumericOptional", "NumericOptional", "NumericOptional" )
-colClasses_read_table<-c("DateDDMMYYYY", "TimeString")
-setClass('DateDDMMYYYY')
-setAs("character","DateDDMMYYYY", function(from) {as.Date(from, format="%d/%m/%Y")})
-setClass('TimeString')
-setAs("character","TimeString", function(from) {sapply(from, function(v) {strptime(v, format="%H:%M:%S")})})
-setClass('NumericOptional')
-setAs("character","NumericOptional", 
-      function(from) {
-        sapply(from, function(v) {
-        if (is.na(v) || v=="?") {
-          as.numeric(NA)
-        } 
-        else {
-          as.numeric(v)
-          }
-        })})
-        
+# load data into memory
 
+require("data.table")
+library(data.table)
 
+if (!file.exists("data/")) {
+  dir.create("data")
+}
+
+dataFile <- "data/household_power_consumption.txt";
+if (!file.exists(dataFile)) {
+  destFile <- "data/household_power_consumption.zip";
+  if (!file.exists(destFile)) {
+    dataFileUrl<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip";
+    
+    # remove "method" parameter in the line below if running on Windows
+    download.file(dataFileUrl, destfile=destFile, method="curl")
+  }
+  unzip(destFile, exdir="./data/")
+}
+
+# fread can't deal with '?' in numeric columns, load data as strings and then convert
+# read.csv2 is much slower
+colClasses_fread_numeric_as_string<-c("character", "character", "character", "character", "character", "character", "character", "character", "character" )
 dataTable <- fread(
   "data/household_power_consumption.txt", 
   header = TRUE,
-  sep=c(";"), 
+  sep=c(";"),  
   na.strings=c("?"), 
-  colClasses=colClasses_fread,
-  verbose=TRUE)
+  colClasses=colClasses_fread_numeric_as_string);
 
-dataFrame <- read.table(
-  "data/household_power_consumption.txt", 
-  header = TRUE,
-  sep=c(";"), 
-  na.strings=c("?"), 
-  colClasses=colClasses_read_table
-  )
+# coerce Date column to Date  
+dataTable[,Date:=as.Date(Date,format="%d/%m/%Y")];
+
+# filter data, keep only what's needed
+dataTable<-dataTable[Date<="2007-02-02"][Date>="2007-02-01"]
+
+# coerce the rest of the data columns
+dataTable[,Time:=lapply(Time, function(v) strptime(v, format="%H:%M:%S"))  ]
+# is there a way to coerce all numeric columns in one call? for now do this one-by-one
+dataTable[,Global_active_power:=as.numeric(Global_active_power)];
+dataTable[,Global_intensity:=as.numeric(Global_intensity)];
+dataTable[,Global_reactive_power:=as.numeric(Global_reactive_power)];
+dataTable[,Sub_metering_1:=as.numeric(Sub_metering_1)];
+dataTable[,Sub_metering_2:=as.numeric(Sub_metering_2)];
+dataTable[,Sub_metering_3:=as.numeric(Sub_metering_3)];
+dataTable[,Voltage:=as.numeric(Voltage)];
+
